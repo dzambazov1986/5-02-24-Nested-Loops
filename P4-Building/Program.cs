2b0057e733ebff036986df2b30145be8aaae5cce
Dimityr <dzambazov1986@gmail.com>
2024-02-07 23:33:41 +0200

    Add project files.

---
+++ P4-Building/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.Design;
+
+int floorsCount = int.Parse(Console.ReadLine());
+int estatesCount = int.Parse(Console.ReadLine());   
+
+for  (int f = floorsCount; f >= 1; f--)
+{
+    string type = "";
+    if (f == floorsCount)
+    {
+        type = "L";
+    }
+    else if (f % 2 == 0)
+    {
+        type = "O";
+    }
+    else type = "A";
+
+    for (int e = 0; e < estatesCount; e++)
+    {
+        Console.Write($"{type}{f}{e} ");
+    }
+    Console.WriteLine();
+}
---
+++ P5-Number Pyramid/Program.cs
@@ -0,0 +1,19 @@
+﻿int n = int.Parse(Console.ReadLine());
+int LineEnd = 1;
+int i = 1;
+
+while (i <= n)
+{
+    for (int pr = 1; pr <= LineEnd; pr++)
+    {
+        Console.Write(i + " ");
+        i++;
+        if (i > n)
+        {
+            break;
+        }
+    }
+
+    Console.WriteLine();
+    LineEnd++;
+}
